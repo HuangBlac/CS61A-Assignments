385069069caeceb6150b726ff26e213d6b41550c
Nanchao Huang <2023302181119@whu.edu.cn>
2026-08-21 17:16:09 +0800

    finish hw02,understand iteration more than before,and a little lambda function,intersting

--- HW/hw02/hw02.py
+++ HW/hw02/hw02.py
@@ -32,6 +32,10 @@ def product(n, term):
     162
     """
     "*** YOUR CODE HERE ***"
+    start = 1
+    for i in range(1, n + 1):
+        start = start * term(i)
+    return start
 
 
 def accumulate(fuse, start, n, term):
@@ -54,6 +58,10 @@ def accumulate(fuse, start, n, term):
     19
     """
     "*** YOUR CODE HERE ***"
+    result = start
+    for i in range(1, n + 1):
+        result = fuse(result, term(i))
+    return result
 
 
 def summation_using_accumulate(n, term):
@@ -68,7 +76,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return summation_using_accumulate(n-1, term) + term(n) if n > 0 else 0
 
 
 def product_using_accumulate(n, term):
@@ -83,7 +91,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return product_using_accumulate(n-1, term) * term(n) if n > 0 else 1
 
 
 def make_repeater(f, n):
@@ -100,4 +108,8 @@ def make_repeater(f, n):
     390625
     """
     "*** YOUR CODE HERE ***"
+    fun = lambda x: f(x)
+    for i in range(n - 1):
+        fun = lambda x, fun=fun: f(fun(x))
+    return fun
 
