ce5e41a87b2750339901da8a72c6d05da8dc51e1
Nanchao Huang <2023302181119@whu.edu.cn>
2026-09-08 23:27:27 +0800

    顺手写了一个题目

--- Lab/lab05/lab05.py
+++ Lab/lab05/lab05.py
@@ -178,6 +178,15 @@ def partial_reverse(s, start):
     [1, 2, 7, 6, 5, 3, 4]
     """
     "*** YOUR CODE HERE ***"
+    i = start
+    j = len(s)-1
+    while i < j:
+        temp = s[i]
+        s[i] = s[j]
+        s[j] = temp
+        i += 1
+        j -= 1
+
 
 
 
